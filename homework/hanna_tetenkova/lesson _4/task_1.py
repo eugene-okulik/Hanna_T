cdead71f5d59ac4fc35da84f73ac9115b4f82c4d
Hanna tetenkova <ann_tetenkova@gmail.com>
2026-05-14 15:53:29 +0200

    homework_4 done third attempt

--- homework/hanna_tetenkova/lesson _4/task_1.py
+++ homework/hanna_tetenkova/lesson _4/task_1.py
@@ -1,15 +1,16 @@
 my_dict = {
-'tuple': (1, 8, False, 'apple', 2.99),
-'list': [5, 9, 'orange', 4.28, True],
-'dict': {
-    'one': 'banana',
-    2: 2,
-    'three': 3.98,
-    'four': [1, 'mango', 8],
-    'five': 'lime'
-},
+    'tuple': (1, 8, False, 'apple', 2.99),
+    'list': [5, 9, 'orange', 4.28, True],
+    'dict': {
+        'one': 'banana',
+        2: 2,
+        'three': 3.98,
+        'four': [1, 'mango', 8],
+        'five': 'lime'
+    },
     'set': {8, 3, 'lemon', True, None}
 }
+
 print(my_dict['tuple'][-1])
 
 my_dict['list'].append(3)
