2cf4489511fa6962eb9922c94611cb162e2d0952
Hanna tetenkova <ann_tetenkova@gmail.com>
2026-06-01 16:59:38 +0200

    homework_5 done second attempt

--- homework/hanna_tetenkova/lesson_5/task_2.py
+++ homework/hanna_tetenkova/lesson_5/task_2.py
@@ -1,14 +1,14 @@
 result_1 = 'результат операции: 42'
 index = result_1.index(':')
 number = int(result_1[index + 1:].strip())
-print (number + 10)
+print(number + 10)
 
 result_2 = 'результат операции: 514'
 index = result_2.index(':')
 number = int(result_2[index + 1:].strip())
-print (number + 10)
+print(number + 10)
 
 result_3 = 'результат работы программы: 9'
 index = result_3.index(':')
 number = int(result_3[index + 1:].strip())
-print (number + 10)
+print(number + 10)
