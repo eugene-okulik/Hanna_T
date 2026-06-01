4cda2c563f49f7ad3a83756f7cd1236260148464
Hanna tetenkova <ann_tetenkova@gmail.com>
2026-06-01 17:02:56 +0200

    homework_5 done third attempt

--- homework/hanna_tetenkova/lesson_5/task_3.py
+++ homework/hanna_tetenkova/lesson_5/task_3.py
@@ -4,7 +4,6 @@ subjects = ['math', 'biology', 'geography']
 students_str = ', '.join(students)
 subjects_str = ', '.join(subjects)
 
-#my_text = 'Students ' + students_str + ' study these subjects: ' + subjects_str
 my_text = f'Students {students_str} study these subjects: {subjects_str}'
 
 print(my_text)
